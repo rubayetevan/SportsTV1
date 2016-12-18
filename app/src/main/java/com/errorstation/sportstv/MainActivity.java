54963afbcf7f17be39f6233191ddd9fb8fe1890f
Rubayet Hassan <rubayet.evan@gmail.com>
2016-12-18 12:08:30 +0600

    tab color font

--- app/src/main/java/com/errorstation/sportstv/MainActivity.java
+++ app/src/main/java/com/errorstation/sportstv/MainActivity.java
@@ -1,29 +1,44 @@
 package com.errorstation.sportstv;
 
+import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar mainTB;
     TabLayout tabLayout;
     ViewPager viewPager;
+    TextView headingTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializer();
+        changeTabsFont();
+    }
+
+    private void initializer() {
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
         mainTB = (Toolbar) findViewById(R.id.mainTB);
         setSupportActionBar(mainTB);
+        getSupportActionBar().setTitle("");
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
         tabLayout.setupWithViewPager(viewPager);
+        headingTV = (TextView) findViewById(R.id.headingTV);
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "kalpurushansi.ttf");
+        headingTV.setTypeface(typeFace);
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -32,5 +47,21 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new ScheduleFragment(), getResources().getString(R.string.schedule));
         adapter.addFragment(new NewsFragment(), getResources().getString(R.string.news));
         viewPager.setAdapter(adapter);
+    }
+
+    private void changeTabsFont() {
+
+        ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
+        int tabsCount = vg.getChildCount();
+        for (int j = 0; j < tabsCount; j++) {
+            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+            int tabChildsCount = vgTab.getChildCount();
+            for (int i = 0; i < tabChildsCount; i++) {
+                View tabViewChild = vgTab.getChildAt(i);
+                if (tabViewChild instanceof TextView) {
+                    ((TextView) tabViewChild).setTypeface(Typeface.createFromAsset(getAssets(), "kalpurushansi.ttf"));
+                }
+            }
+        }
     }
 }
