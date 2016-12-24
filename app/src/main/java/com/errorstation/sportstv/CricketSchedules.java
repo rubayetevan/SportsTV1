e2b52f8ae467d00783d24050139161bfafbc5ccd
Rubayet Hassan <rubayet.evan@gmail.com>
2016-12-24 17:45:15 +0600

    qq

---
+++ app/src/main/java/com/errorstation/sportstv/CricketSchedules.java
@@ -0,0 +1,33 @@
+
+package com.errorstation.sportstv;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CricketSchedules {
+
+    @SerializedName("success")
+    @Expose
+    private String success;
+    @SerializedName("matchinfo")
+    @Expose
+    private List<Matchinfo> matchinfo = null;
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
+    public List<Matchinfo> getMatchinfo() {
+        return matchinfo;
+    }
+
+    public void setMatchinfo(List<Matchinfo> matchinfo) {
+        this.matchinfo = matchinfo;
+    }
+
+}
---
+++ app/src/main/java/com/errorstation/sportstv/SchedulesAdapter.java
@@ -0,0 +1,96 @@
+package com.errorstation.sportstv;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Rubayet on 24-Dec-16.
+ */
+
+public class SchedulesAdapter extends RecyclerView.Adapter<SchedulesAdapter.SchedulesHolder> {
+
+  Context context;
+  List<Matchinfo> schedulesList = new ArrayList<Matchinfo>();
+  Activity activity;
+  Typeface typeFace;
+
+  public SchedulesAdapter(Context context, List<Matchinfo> schedulesList, Activity activity) {
+    this.context = context;
+    this.schedulesList = schedulesList;
+    this.activity = activity;
+    typeFace = Typeface.createFromAsset(context.getAssets(), "Siyamrupali.ttf");
+  }
+
+  @Override public SchedulesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View itemView =
+        LayoutInflater.from(parent.getContext()).inflate(R.layout.schedules_list, parent, false);
+    return new SchedulesAdapter.SchedulesHolder(itemView);
+  }
+
+  @Override public void onBindViewHolder(SchedulesHolder holder, int position) {
+    holder.teamsTV.setTypeface(typeFace);
+    holder.matchTV.setTypeface(typeFace);
+    holder.placeTV.setTypeface(typeFace);
+    holder.timeTV.setTypeface(typeFace);
+
+
+
+
+    holder.teamsTV.setText(schedulesList.get(position).getTeam1()+" বনাম "+schedulesList.get(position).getTeam2());
+    holder.matchTV.setText(schedulesList.get(position).getMatchno()+"ম খেলা, "+schedulesList.get(position).getMatchtype()+", " +schedulesList.get(position).getStartdate());
+    holder.placeTV.setText(schedulesList.get(position).getVenue());
+    holder.timeTV.setText(schedulesList.get(position).getStarttime());
+    Glide
+        .with(context)
+        .load(schedulesList.get(position).getTeam1logo())
+        .placeholder(R.drawable.ic_tv_channel_icon)
+        .crossFade()
+        .into(holder.team1LogoIMGV);
+    Glide
+        .with(context)
+        .load(schedulesList.get(position).getTeam2logo())
+        .placeholder(R.drawable.ic_tv_channel_icon)
+        .crossFade()
+        .into(holder.team2LogoIMGV);
+
+
+
+
+  }
+
+  @Override public int getItemCount() {
+    return schedulesList.size();
+  }
+
+  public class SchedulesHolder extends RecyclerView.ViewHolder {
+
+    public ImageView team1LogoIMGV;
+    public ImageView team2LogoIMGV;
+    public TextView teamsTV;
+    public TextView matchTV;
+    public TextView placeTV;
+    public TextView timeTV;
+
+    public SchedulesHolder(View itemView) {
+      super(itemView);
+      team1LogoIMGV = (ImageView) itemView.findViewById(R.id.team1LogoIMGV);
+      team2LogoIMGV = (ImageView) itemView.findViewById(R.id.team2LogoIMGV);
+      teamsTV = (TextView) itemView.findViewById(R.id.teamsTV);
+      matchTV = (TextView) itemView.findViewById(R.id.matchTV);
+      placeTV = (TextView) itemView.findViewById(R.id.placeTV);
+      timeTV = (TextView) itemView.findViewById(R.id.timeTV);
+    }
+  }
+}
